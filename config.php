21ddfd1f6c477d682000329a5b34c5bd3fabd9c1
IsaacZanni <isaac.fernandes4@etec.sp.gov.br>
2024-09-03 08:47:10 -0300

    mexi na página config

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'form_exemplo';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    // if($conexao->connect_errno){
+    //     echo "Erro";
+    // }
+    // else{
+    //     echo "Conectado com sucesso";
+    // }
+
+    
+?>
